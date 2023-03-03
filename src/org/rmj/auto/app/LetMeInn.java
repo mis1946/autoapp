64a3cd61a1d11d6b01ead6bc70278118da17028c
Michael Cuison <michael_cuison07@yahoo.com>
2023-03-03 15:11:22 +0800

    Initial commit

---
+++ src/org/rmj/auto/app/LetMeInn.java
@@ -0,0 +1,23 @@
+package org.rmj.auto.app.views;
+
+import org.rmj.appdriver.GRider;
+
+public class LetMeInn {
+    public static void main(String [] args){
+        String path;
+        if(System.getProperty("os.name").toLowerCase().contains("win")){
+            path = "D:/GGC_Java_Systems";
+        }
+        else{
+            path = "/srv/GGC_Java_Systems";
+        }
+        System.setProperty("sys.default.path.config", path);
+        
+        GRider instance = new GRider();
+        
+        if (!instance.logUser("AutoApp", "M001111122")){
+            System.err.println(instance.getMessage() + instance.getErrMsg());
+            System.exit(1);
+        }
+    }
+}

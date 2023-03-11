1c15897b63f33291849513da2a682e1c71767862
r-sie <32829606+Arsiela@users.noreply.github.com>
2023-03-11 17:23:50 +0800

    Table Client Email
    For populating Table Client Email

---
+++ src/org/rmj/auto/app/views/TableClientEmail.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.rmj.auto.app.views;
+
+import javafx.beans.property.SimpleStringProperty;
+
+/**
+ * Date: 03-07-2023
+ * @author Arsiela
+ */
+public class TableClientEmail {
+     private SimpleStringProperty emadindex01;
+     private SimpleStringProperty emadindex02; 
+     private SimpleStringProperty emadindex03;
+     private SimpleStringProperty xEmailID;
+     
+     TableClientEmail( String emadindex01,
+                         String emadindex02,
+                         String emadindex03,
+                         String xEmailID){
+     
+          this.emadindex01 = new SimpleStringProperty(emadindex01);
+          this.emadindex02 = new SimpleStringProperty(emadindex02);
+          this.emadindex03 = new SimpleStringProperty(emadindex03);
+          this.xEmailID = new SimpleStringProperty(xEmailID);
+     }
+     
+     public String getxEmailID() {
+          return xEmailID.get();
+     }
+
+     public void setxEmailID(String xEmailID) {
+          this.xEmailID.set(xEmailID);
+     }
+     
+     public String getEmadindex01(){return emadindex01.get();}
+     public void setEmadindex01(String emadindex01){this.emadindex01.set(emadindex01);}
+     
+     public String getEmadindex02(){return emadindex02.get();}
+     public void setEmadindex02(String emadindex02){this.emadindex02.set(emadindex02);}
+     
+     public String getEmadindex03(){return emadindex03.get();}
+     public void setEmadindex03(String emadindex03){this.emadindex03.set(emadindex03);}
+    
+     
+}

ec8dbe2d4cfb2be2786890e1d1c9505a43f7d680
Davee <johndaveaquino35@gmail.com>
2023-04-27 17:14:33 +0800

    Update VehicleSalesApprovalController.java

--- src/org/rmj/auto/app/sales/VehicleSalesApprovalController.java
+++ src/org/rmj/auto/app/sales/VehicleSalesApprovalController.java
@@ -4,25 +4,37 @@
  */
 package org.rmj.auto.app.sales;
 
+import com.sun.javafx.scene.control.skin.TableHeaderRow;
 import java.net.URL;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import java.util.ResourceBundle;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.value.ChangeListener;
 import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.CheckBoxTableCell;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import javafx.util.Callback;
 import org.rmj.appdriver.GRider;
 import org.rmj.appdriver.agentfx.ShowMessageFX;
+import org.rmj.auto.app.bank.BankEntryTableList;
 import org.rmj.auto.app.views.ScreenInterface;
 import org.rmj.auto.app.views.unloadForm;
 
@@ -40,32 +52,23 @@ public class VehicleSalesApprovalController implements Initializable,ScreenInter
     
     @FXML
     private AnchorPane AnchorMain;
-    @FXML
-    private Button btnClose;
+    
     
     ObservableList<String> cFilter = FXCollections.observableArrayList("Advance Slip Date", "Advances Slip No.", "Advances Type",
                                                                        "Customer Name","Employee Name","Unit Description");
         
-    ObservableList<String> cType = FXCollections.observableArrayList("Reservation", "Deposit");
+    ObservableList<String> cType = FXCollections.observableArrayList("Reservation","Deposit");
     
-    @FXML
-    private TableView tblVehicleApproval;
-    @FXML
-    private TableColumn tblindexRow;
-    @FXML
-    private TableColumn tblindex02;
-    @FXML
-    private TableColumn tblindex03;
-    @FXML
-    private TableColumn tblindex17;
-    @FXML
-    private TableColumn tblindex16;
+    private ObservableList<VehicleSalesApprovalTable> vhlApprovalData = FXCollections.observableArrayList();
+    private FilteredList<VehicleSalesApprovalTable> filteredData;
     @FXML
     private Button btnApproved;
     @FXML
     private Button btnFilter;
     @FXML
     private Button btnRefresh;
+    @FXML
+    private Button btnClose;
     @FXML
     private TextField txtFieldSearch;
     @FXML
@@ -80,12 +83,48 @@ public class VehicleSalesApprovalController implements Initializable,ScreenInter
     private Label lFrom;
     @FXML
     private Label lTo;
+    @FXML
+    private TableView<VehicleSalesApprovalTable> tblVhclApproval;
+    @FXML
+    private TableColumn <VehicleSalesApprovalTable, String>tblRow;
+    @FXML
+    private TableColumn<VehicleSalesApprovalTable, Boolean> tblselected;
+    @FXML
+    private TableColumn<VehicleSalesApprovalTable, String> tblslipNo;
+    @FXML
+    private TableColumn<VehicleSalesApprovalTable, String> tbltype;
+    @FXML
+    private TableColumn<VehicleSalesApprovalTable, String> tblcustomerName;
+    @FXML
+    private TableColumn<VehicleSalesApprovalTable, String> tblunitDescription;
+    @FXML
+    private TableColumn<VehicleSalesApprovalTable, String> tblamount;
+    @FXML
+    private TableColumn<VehicleSalesApprovalTable, String> tblseName;
+    @FXML
+    private TableColumn<VehicleSalesApprovalTable, LocalDate> tblbranch;
+    @FXML
+    private CheckBox selectAllCheckBox;
+    @FXML
+    private TableColumn<VehicleSalesApprovalTable, String> tblslipDate;
 
-    /**
-     * Initializes the controller class.
-     */
+     
+    ObservableList<VehicleSalesApprovalTable> slipDataList = FXCollections.observableArrayList();
+    // Create sample SlipData objects
+    VehicleSalesApprovalTable slipData1 = new VehicleSalesApprovalTable("1","001", "Reservation","2023, 1, 20", "John Doe",
+        "Unit A", "1000.0", "SE 1", "Branch 1");
+    VehicleSalesApprovalTable slipData2 = new VehicleSalesApprovalTable("2","002", "Deposit","2023, 7, 29","Jane Smith",
+        "Unit B", "2000.0", "SE 2", "Branch 2");
+     VehicleSalesApprovalTable slipData3 = new VehicleSalesApprovalTable("3","003", "Reservation","2023, 4, 28","Dave Smith",
+        "Unit C", "3000.0", "SE 3", "Branch 3");
+  
+
+    
     @Override
-    public void initialize(URL url, ResourceBundle rb) {        
+    public void initialize(URL url, ResourceBundle rb) { 
+        
+        filteredData = new FilteredList<>(tblVhclApproval.getItems());
+       
         txtFieldSearch.setVisible(false);
         txtFieldSearch.setManaged(false);
         lFrom.setManaged(false);
@@ -103,8 +142,48 @@ public class VehicleSalesApprovalController implements Initializable,ScreenInter
         comboFilter.setItems(cFilter);
         comboType.setItems(cType);
         //cmdButtons_click properties
+        
+        slipDataList.addAll(slipData1, slipData2, slipData3);
+        loadVhlApprovalTable();
+        tblVhclApproval.setItems(slipDataList);
         btnClose.setOnAction(this::cmdButton_Click);
+        btnApproved.setOnAction(this::cmdButton_Click);
+        btnFilter.setOnAction(this::cmdButton_Click);
+
+        
     }    
+   
+        private void loadVhlApprovalTable(){
+             //try {
+                  /*Populate table*/
+             
+                 vhlApprovalData.clear();
+                 
+                       for (int i = 1; i <= 5; i++){
+          
+                          
+                            vhlApprovalData.add(new VehicleSalesApprovalTable(
+                            String.valueOf(i), //ROW
+                            "",
+                                    "", 
+                                    "",
+                                    "",
+                                    "",
+                                    "",
+                                    "",
+                                    ""
+
+                            ));
+                       }
+                       initVhlApprovalTable();
+
+                
+               
+//          } catch (SQLException e) {
+//               ShowMessageFX.Warning(getStage(),e.getMessage(), "Warning", null);
+//          }
+     }
+    
     private void cmdButton_Click(ActionEvent event) {
     String lsButton = ((Button)event.getSource()).getId();
     switch(lsButton){
@@ -119,6 +198,28 @@ public class VehicleSalesApprovalController implements Initializable,ScreenInter
                 return;
             }
             break;
+        case "btnFilter":
+                System.out.println("FILTER!");
+                String selectedValue = comboType.getValue();
+
+                // Apply the filter predicate based on the selected value
+                filteredData.setPredicate(clients -> {
+                    if (selectedValue == null || selectedValue.isEmpty()) {
+                        // No filter selected, show all data
+                        return true;
+                    } else {
+                        // Filter based on selected value (e.g., "Reservation" or "Deposit")
+                        return clients.getTbltype().equalsIgnoreCase(selectedValue);
+                    }
+                });
+
+          tblVhclApproval.setItems(filteredData);
+    
+        break;
+            
+        case "btnApproved":
+                System.out.println("THIS IS APPROVED BUTTON!");
+            break;
 //        case "btnFilter": //filter table
 //            // Retrieve filter criteria from text fields, combo box, and date pickers
 //            String customerFilter = txtFieldSearch.getText().trim().toLowerCase();
@@ -126,7 +227,7 @@ public class VehicleSalesApprovalController implements Initializable,ScreenInter
 //            String typeFilter = comboType.getValue();
 //            LocalDate fromDateFilter = fromDate.getValue();
 //            LocalDate toDateFilter = toDate.getValue();
-//
+//  
 //            // Create a list to hold the filtered orders
 //            List<Order> filteredOrders = new ArrayList<>();
 //
@@ -163,7 +264,7 @@ public class VehicleSalesApprovalController implements Initializable,ScreenInter
         comboFilter.setOnAction(e -> {
             String selectedFilter = comboFilter.getSelectionModel().getSelectedItem();
 
-            // Hide all controls first
+            // Hide all controls first  
             txtFieldSearch.setVisible(false);
             txtFieldSearch.setManaged(false);
             lFrom.setManaged(false);
@@ -212,9 +313,43 @@ public class VehicleSalesApprovalController implements Initializable,ScreenInter
             }
         });
     }
+
+ 
     @Override
     public void setGRider(GRider foValue) {
           oApp = foValue;
      }    
  
+      private void initVhlApprovalTable() {
+          tblRow.setCellValueFactory(new PropertyValueFactory<>("tblRow"));  //Row
+          tblselected.setCellValueFactory(new PropertyValueFactory<>("select"));
+          selectAllCheckBox.selectedProperty().addListener(new ChangeListener<Boolean>(){
+              @Override
+              public void changed(javafx.beans.value.ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+                    ObservableList<VehicleSalesApprovalTable> items = tblVhclApproval.getItems();
+                     
+                    for(VehicleSalesApprovalTable item : items){
+                        {
+                            if(selectAllCheckBox.isSelected()){
+                                item.getSelect().setSelected(true);
+                            }
+                            else{
+                                item.getSelect().setSelected(false);
+                            }
+                        }
+                    }
+              }
+          });
+          
+          tblslipNo.setCellValueFactory(new PropertyValueFactory<>("tblslipNo")); 
+          tbltype.setCellValueFactory(new PropertyValueFactory<>("tbltype")); 
+          tblslipDate.setCellValueFactory(new PropertyValueFactory<>("tblslipDate"));
+    
+          tblcustomerName.setCellValueFactory(new PropertyValueFactory<>("tblcustomerName")); 
+          tblunitDescription.setCellValueFactory(new PropertyValueFactory<>("tblunitDescription")); 
+          tblamount.setCellValueFactory(new PropertyValueFactory<>("tblamount")); 
+          tblseName.setCellValueFactory(new PropertyValueFactory<>("tblseName")); 
+          tblbranch.setCellValueFactory(new PropertyValueFactory<>("tblbranch"));
+          
+      }
 }

d7c3f6a7fed6ce7f94cea0928d8a8791a82cb1af
Davee <johndaveaquino35@gmail.com>
2023-05-18 17:25:27 +0800

    Create TownCityMainEntryDialogController.java

---
+++ src/org/rmj/auto/app/views/TownCityMainEntryDialogController.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package org.rmj.auto.app.views;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+/**
+ * FXML Controller class
+ *
+ * @author User
+ */
+public class TownCityMainEntryDialogController implements Initializable {
+
+    @FXML
+    private Button btnAdd;
+    @FXML
+    private Button btnAdd1;
+    @FXML
+    private Button btnClose;
+    @FXML
+    private Label lblClientName;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}

fac70d93cfa365c4ea268b0934eebb0e513c7e8b
Davee <johndaveaquino35@gmail.com>
2023-05-22 17:24:51 +0800

    Update ActivityMemberEntryDialogController.java

--- src/org/rmj/auto/app/views/ActivityMemberEntryDialogController.java
+++ src/org/rmj/auto/app/views/ActivityMemberEntryDialogController.java
@@ -6,21 +6,52 @@ package org.rmj.auto.app.views;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import org.rmj.appdriver.GRider;
+import org.rmj.appdriver.agentfx.CommonUtils;
+import org.rmj.appdriver.agentfx.ShowMessageFX;
 
 /**
  * FXML Controller class
  *
  * @author User
  */
-public class ActivityMemberEntryDialogController implements Initializable {
-
+public class ActivityMemberEntryDialogController implements Initializable,ScreenInterface {
+    private GRider oApp;
+    @FXML
+    private Button btnAdd;
+    @FXML
+    private Button btnClose;
+    unloadForm unload = new unloadForm(); //Used in Close Button
+    private final String pxeModuleName = "ActivityMemberEntryDialogController"; //Form Title
+    @FXML
+    private AnchorPane AnchorMain;
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+      
+        btnClose.setOnAction(this::cmdButton_Click);
     }    
+
+    @Override
+    public void setGRider(GRider foValue) {
+        oApp = foValue;
+    }   
     
+     private void cmdButton_Click(ActionEvent event) {
+      
+            String lsButton = ((Button)event.getSource()).getId();
+            switch (lsButton){
+                  case "btnClose":
+                    CommonUtils.closeStage(btnClose);
+                    break;
+            }
+     }
 }
+            

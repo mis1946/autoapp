03103ae4c86d320888b0c9da0dcd41906617879f
Davee <johndaveaquino35@gmail.com>
2023-06-15 16:52:23 +0800

    Create ActivityTableList.java

---
+++ src/org/rmj/auto/app/views/ActivityTableList.java
@@ -0,0 +1,172 @@
+///*
+// * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+// * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+// */
+package org.rmj.auto.app.views;
+
+import javafx.beans.property.SimpleStringProperty;
+
+/**
+ *
+ * @author John Dave
+ */
+public class ActivityTableList {
+
+    private SimpleStringProperty tblindex01;
+    private SimpleStringProperty tblindex02;
+    private SimpleStringProperty tblindex03;
+    private SimpleStringProperty tblindex05;
+    private SimpleStringProperty tblindex06;
+    private SimpleStringProperty tblindex07;
+    private SimpleStringProperty tblindex08;
+    private SimpleStringProperty tblindex09;
+    private SimpleStringProperty tblindex10;
+    private SimpleStringProperty tblindex12;
+    private SimpleStringProperty tblindex15;
+    private SimpleStringProperty tblindex16;
+    private SimpleStringProperty tblindex19;
+    private SimpleStringProperty tblindex21;
+
+    public ActivityTableList(String tblindex01,
+            String tblindex02,
+            String tblindex03,
+            String tblindex05,
+            String tblindex06,
+            String tblindex07,
+            String tblindex08,
+            String tblindex09,
+            String tblindex10,
+            String tblindex12,
+            String tblindex15,
+            String tblindex16,
+            String tblindex19,
+            String tblindex21) {
+        this.tblindex01 = new SimpleStringProperty(tblindex01);
+        this.tblindex02 = new SimpleStringProperty(tblindex02);
+        this.tblindex03 = new SimpleStringProperty(tblindex03);
+        this.tblindex05 = new SimpleStringProperty(tblindex05);
+        this.tblindex06 = new SimpleStringProperty(tblindex06);
+        this.tblindex07 = new SimpleStringProperty(tblindex07);
+        this.tblindex08 = new SimpleStringProperty(tblindex08);
+        this.tblindex09 = new SimpleStringProperty(tblindex09);
+        this.tblindex10 = new SimpleStringProperty(tblindex10);
+        this.tblindex12 = new SimpleStringProperty(tblindex12);
+        this.tblindex15 = new SimpleStringProperty(tblindex15);
+        this.tblindex16 = new SimpleStringProperty(tblindex16);
+        this.tblindex19 = new SimpleStringProperty(tblindex19);
+        this.tblindex21 = new SimpleStringProperty(tblindex21);
+    }
+
+    public String getTblindex01() {
+        return tblindex01.get();
+    }
+
+    public void setTblindex01(String tblindex01) {
+        this.tblindex01.set(tblindex01);
+    }
+
+    public String getTblindex02() {
+        return tblindex02.get();
+    }
+
+    public void setTblindex02(String tblindex02) {
+        this.tblindex01.set(tblindex02);
+    }
+
+    public String getTblindex03() {
+        return tblindex03.get();
+    }
+
+    public void setTblindex03(String tblindex03) {
+        this.tblindex01.set(tblindex03);
+    }
+
+    public String getTblindex05() {
+        return tblindex05.get();
+    }
+
+    public void setTblindex05(String tblindex05) {
+        this.tblindex01.set(tblindex05);
+    }
+
+    public String getTblindex06() {
+        return tblindex06.get();
+    }
+
+    public void setTblindex06(String tblindex06) {
+        this.tblindex01.set(tblindex06);
+    }
+
+    public String getTblindex07() {
+        return tblindex07.get();
+    }
+
+    public void setTblindex07(String tblindex07) {
+        this.tblindex01.set(tblindex07);
+    }
+
+    public String getTblindex08() {
+        return tblindex08.get();
+    }
+
+    public void setTblindex08(String tblindex08) {
+        this.tblindex01.set(tblindex08);
+    }
+
+    public String getTblindex09() {
+        return tblindex09.get();
+    }
+
+    public void setTblindex09(String tblindex09) {
+        this.tblindex01.set(tblindex09);
+    }
+
+    public String getTblindex10() {
+        return tblindex10.get();
+    }
+
+    public void setTblindex10(String tblindex10) {
+        this.tblindex01.set(tblindex10);
+    }
+
+    public String getTblindex12() {
+        return tblindex12.get();
+    }
+
+    public void setTblindex12(String tblindex12) {
+        this.tblindex01.set(tblindex12);
+    }
+
+    public String getTblindex15() {
+        return tblindex15.get();
+    }
+
+    public void setTblindex15(String tblindex15) {
+        this.tblindex01.set(tblindex15);
+    }
+
+    public String getTblindex16() {
+        return tblindex16.get();
+    }
+
+    public void setTblindex16(String tblindex16) {
+        this.tblindex01.set(tblindex16);
+    }
+
+    public String getTblindex19() {
+        return tblindex19.get();
+    }
+
+    public void setTblindex19(String tblindex19) {
+        this.tblindex01.set(tblindex19);
+    }
+
+    public String getTblindex21() {
+        return tblindex21.get();
+    }
+
+    public void setTblindex21(String tblindex21) {
+        this.tblindex01.set(tblindex21);
+    }
+
+}

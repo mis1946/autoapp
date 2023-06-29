1604203675acb49a58ee323c82d54b02a8b7b981
Davee <johndaveaquino35@gmail.com>
2023-06-29 16:04:06 +0800

    Update BinEntryParamController.java

--- src/org/rmj/auto/app/parts/BinEntryParamController.java
+++ src/org/rmj/auto/app/parts/BinEntryParamController.java
@@ -7,6 +7,8 @@ package org.rmj.auto.app.parts;
 import java.net.URL;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.beans.property.ReadOnlyBooleanPropertyBase;
 import javafx.beans.value.ChangeListener;
 import javafx.event.ActionEvent;
@@ -23,6 +25,7 @@ import org.rmj.appdriver.callback.MasterCallback;
 import org.rmj.appdriver.constants.EditMode;
 import org.rmj.auto.app.views.ScreenInterface;
 import org.rmj.auto.app.views.ScreenInterface;
+import org.rmj.auto.parts.parameters.PartsBin;
 
 /**
  * FXML Controller class
@@ -34,7 +37,7 @@ public class BinEntryParamController implements Initializable, ScreenInterface {
     private MasterCallback oListener;
     private final String pxeModuleName = "Bin Entry Form";
     private int pnEditMode;//Modifying fields
-//    private Warehouse oTrans;
+    private PartsBin oTrans;
     private GRider oApp;
     @FXML
     private Button btnClose;
@@ -60,9 +63,9 @@ public class BinEntryParamController implements Initializable, ScreenInterface {
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-//        oTrans = new Bin(oApp, oApp.getBranchCode(), true); //Initialize ClientMaster
-//        oTrans.setCallback(oListener);
-//        oTrans.setWithUI(true);
+        oTrans = new PartsBin(oApp, oApp.getBranchCode(), true); //Initialize ClientMaster
+        oTrans.setCallback(oListener);
+        oTrans.setWithUI(true);
 
         setCapsLockBehavior(txtField02);
         txtField02.focusedProperty().addListener(txtField_Focus);
@@ -91,92 +94,131 @@ public class BinEntryParamController implements Initializable, ScreenInterface {
     }
 
     private void loadBinField() {
-//        txtField01.setText((String) oTrans.getMaster(2)); //
-//        txtField02.setText((String) oTrans.getMaster(2)); //
-//        chboxActivate.setSelected(oTrans.getMaster(2).toString());
+        try {
+            txtField01.setText((String) oTrans.getMaster(1)); //
+            txtField02.setText((String) oTrans.getMaster(2)); //
+            if (oTrans.getMaster(3).toString().equals("1")) {
+                cboxActivate.setSelected(true);
+            } else {
+                cboxActivate.setSelected(false);
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(BinEntryParamController.class.getName()).log(Level.SEVERE, null, ex);
+        }
 
     }
 
     private void cmdButton_Click(ActionEvent event) {
-        String lsButton = ((Button) event.getSource()).getId();
-        switch (lsButton) {
-            case "btnAdd": //create
-                System.out.println("hello");
-//                if (oTrans.NewRecord()) {
-//                    try {
-//                        loadBinField();
-//                        clearFields();
-//                        pnEditMode = oTrans.getEditMode();
-//                    } catch (SQLException ex) {
-//                        Logger.getLogger(BinEntryParamController.class.getName()).log(Level.SEVERE, null, ex);
-//                    }
-//                } else {
-//                    ShowMessageFX.Warning(getStage(), oTrans.getMessage(), "Warning", null);
-//                }
-                break;
-            case "btnSave":
-//                if (ShowMessageFX.OkayCancel(null, pxeModuleName, "Are you sure, do you want to save?") == true) {
-//                    if (oTrans.SaveRecord()) {
-//                        ShowMessageFX.Information(null, pxeModuleName, "New bin added sucessfully.");
-//                        pnEditMode = oTrans.getEditMode();
-//                    } else {
-//                        ShowMessageFX.Warning(null, pxeModuleName, oTrans.getMessage());
-//                        return;
-//                    }
-//                }
-                break;
-            case "btnEdit":
-                break;
+        try {
+            String lsButton = ((Button) event.getSource()).getId();
+            switch (lsButton) {
+                case "btnAdd": //create
+                    if (oTrans.NewRecord()) {
+                        clearFields();
+                        loadBinField();
+                        pnEditMode = oTrans.getEditMode();
+                    } else {
+                        ShowMessageFX.Warning(getStage(), oTrans.getMessage(), "Warning", null);
+                    }
+                    break;
+                case "btnSave":
+                    if (ShowMessageFX.OkayCancel(null, pxeModuleName, "Are you sure, do you want to save?") == true) {
+                        if (txtField02.getText().trim().equals("")) {
+                            ShowMessageFX.Warning(getStage(), "Please enter a value for bin description", "Warning", null);
+                            txtField02.requestFocus();
+                            return;
+                        }
+                        if (oTrans.SaveRecord()) {
+                            ShowMessageFX.Information(null, pxeModuleName, "New bin added sucessfully.");
+                            if (oTrans.OpenRecord(oTrans.getMaster(1).toString())) {
+                                loadBinField();
+                                pnEditMode = oTrans.getEditMode();
+                            }
+                        } else {
+                            ShowMessageFX.Warning(null, pxeModuleName, oTrans.getMessage());
+                            return;
+                        }
+                    }
+                    break;
+                case "btnEdit":
+                    if (oTrans.UpdateRecord()) {
+                        pnEditMode = oTrans.getEditMode();
+                    } else {
+                        ShowMessageFX.Warning(getStage(), oTrans.getMessage(), "Warning", null);
+                    }
+                    break;
 
-            case "btnCancel":
-                if (ShowMessageFX.OkayCancel(getStage(), "Are you sure you want to cancel?", pxeModuleName, null) == true) {
-                    clearFields();
-                    pnEditMode = EditMode.UNKNOWN;
-                }
-                break;
-            case "btnDeactivate":
-                break;
-            case "btnSearch":
-//                try {
-//                if (oTrans.searchEventType()) {
-//                    pnEditMode = EditMode.READY;
-//                }
-//            } catch (SQLException ex) {
-//                Logger.getLogger(BinEntryParamController.class.getName()).log(Level.SEVERE, null, ex);
-//            }
-                break;
-            case "btnClose":
-                CommonUtils.closeStage(btnClose);
-                break;
-            default:
-                ShowMessageFX.Warning(null, pxeModuleName, "Button with name " + lsButton + " not registered.");
-                break;
-        }
-        initButton(pnEditMode);
-    }
-    final ChangeListener<? super Boolean> txtField_Focus = (o, ov, nv) -> {
-//        try {
-        TextField txtField = (TextField) ((ReadOnlyBooleanPropertyBase) o).getBean();
-        int lnIndex = Integer.parseInt(txtField.getId().substring(8, 10));
-        String lsValue = txtField.getText().toUpperCase();
+                case "btnCancel":
+                    if (ShowMessageFX.OkayCancel(getStage(), "Are you sure you want to cancel?", pxeModuleName, null) == true) {
+                        clearFields();
+                        pnEditMode = EditMode.UNKNOWN;
+                    }
+                    break;
+                case "btnDeactivate":
+                    if (ShowMessageFX.OkayCancel(null, pxeModuleName, "Are you sure, do you want to change status?") == true) {
+                        String fsValue = oTrans.getMaster(1).toString();
+                        boolean fbStatus = false;
+                        if (btnDeactivate.getText().equals("Activate")) {
+                            fbStatus = true;
+                        } else {
+                            fbStatus = false;
+                        }
+                        if (oTrans.UpdateRecordStatus(fsValue, fbStatus)) {
+                            ShowMessageFX.Information(getStage(), oTrans.getMessage(), pxeModuleName, null);
+                            if (oTrans.OpenRecord(oTrans.getMaster(1).toString())) {
+                                loadBinField();
+                                pnEditMode = oTrans.getEditMode();
+                            }
+                        } else {
+                            ShowMessageFX.Information(getStage(), "Failed to update status.", pxeModuleName, null);
+                            return;
+                        }
+                    }
+                    break;
 
-        if (lsValue == null) {
-            return;
-        }
-        if (!nv) {
-            /* Lost Focus */
-            switch (lnIndex) {
-                case 2: //
-//                        oTrans.setMaster(lnIndex, lsValue);
+                case "btnSearch":
+                    if (oTrans.searchRecord()) {
+                        loadBinField();
+                        pnEditMode = EditMode.READY;
+                    }
+                    break;
+                case "btnClose":
+                    CommonUtils.closeStage(btnClose);
+                    break;
+                default:
+                    ShowMessageFX.Warning(null, pxeModuleName, "Button with name " + lsButton + " not registered.");
                     break;
             }
-        } else {
-            txtField.selectAll();
+            initButton(pnEditMode);
+        } catch (SQLException ex) {
+            Logger.getLogger(BinEntryParamController.class.getName()).log(Level.SEVERE, null, ex);
         }
-//        } catch (SQLException ex) {
-//            Logger.getLogger(BinEntryParamController.class
-//                    .getName()).log(Level.SEVERE, null, ex);
-//        }
+    }
+    final ChangeListener<? super Boolean> txtField_Focus = (o, ov, nv) -> {
+        try {
+            TextField txtField = (TextField) ((ReadOnlyBooleanPropertyBase) o).getBean();
+            int lnIndex = Integer.parseInt(txtField.getId().substring(8, 10));
+            String lsValue = txtField.getText().toUpperCase();
+
+            if (lsValue == null) {
+                return;
+            }
+            if (!nv) {
+                /* Lost Focus */
+                switch (lnIndex) {
+                    case 2:
+                        oTrans.setMaster(lnIndex, lsValue);
+                        break;
+                }
+            } else {
+                txtField.selectAll();
+
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(BinEntryParamController.class
+                    .getName()).log(Level.SEVERE, null, ex);
+        }
     };
 
     private void clearFields() {
@@ -192,9 +234,9 @@ public class BinEntryParamController implements Initializable, ScreenInterface {
     private void initButton(int fnValue) {
         pnRow = 0;
         boolean lbShow = (fnValue == EditMode.ADDNEW || fnValue == EditMode.UPDATE);
-        txtField02.setDisable(true);
+        txtField01.setDisable(true);
         txtField02.setDisable(!lbShow);
-        cboxActivate.setDisable(!lbShow);
+        cboxActivate.setDisable(true);
         btnAdd.setVisible(!lbShow);
         btnAdd.setManaged(!lbShow);
         btnCancel.setVisible(lbShow);
@@ -203,9 +245,25 @@ public class BinEntryParamController implements Initializable, ScreenInterface {
         btnSave.setManaged(lbShow);
         btnEdit.setVisible(false);
         btnEdit.setManaged(false);
-        if (fnValue == EditMode.READY) { //show edit if user clicked save / browse
-            btnEdit.setVisible(true);
-            btnEdit.setManaged(true);
+        btnDeactivate.setVisible(false);
+        btnDeactivate.setManaged(false);
+        if (fnValue == EditMode.READY) {
+            try {
+                //show edit if user clicked save / browse
+                if (oTrans.getMaster(3).toString().equals("1")) {
+                    btnDeactivate.setText("Deactivate");
+                    btnDeactivate.setVisible(true);
+                    btnDeactivate.setManaged(true);
+                    btnEdit.setVisible(true);
+                    btnEdit.setManaged(true);
+                } else {
+                    btnDeactivate.setText("Activate");
+                    btnDeactivate.setVisible(true);
+                    btnDeactivate.setManaged(true);
+                }
+            } catch (SQLException ex) {
+                Logger.getLogger(BinEntryParamController.class.getName()).log(Level.SEVERE, null, ex);
+            }
 
         }
     }

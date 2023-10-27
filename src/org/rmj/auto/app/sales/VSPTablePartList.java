3577c278f75f0c4aaf53b2dc80fd1b644faa40ec
myst <johndaveaquino35@gmail.com>
2023-10-27 16:49:46 +0800

    Update VSPTablePartList.java

--- src/org/rmj/auto/app/sales/VSPTablePartList.java
+++ src/org/rmj/auto/app/sales/VSPTablePartList.java
@@ -16,29 +16,30 @@ public class VSPTablePartList {
 
     private SimpleStringProperty tblPartsRow;
     private SimpleStringProperty tblindex01_Part;
+    private SimpleStringProperty tblindex03_Part;
     private SimpleStringProperty tblindex14_Part;
     private SimpleStringProperty tblindex09_Part;
     private SimpleStringProperty tblindex08_Part;
     private SimpleStringProperty tblindex05_Part;
     private SimpleStringProperty tblindex06_Part;
-    private SimpleStringProperty tblindex11_Part;
 
     VSPTablePartList(String tblPartsRow,
             String tblindex01_Part,
+            String tblindex03_Part,
             String tblindex14_Part,
             String tblindex09_Part,
             String tblindex08_Part,
             String tblindex06_Part,
-            String tblindex05_Part,
-            String tblindex11_Part) {
+            String tblindex05_Part
+    ) {
         this.tblPartsRow = new SimpleStringProperty(tblPartsRow);
         this.tblindex01_Part = new SimpleStringProperty(tblindex01_Part);
+        this.tblindex03_Part = new SimpleStringProperty(tblindex03_Part);
         this.tblindex14_Part = new SimpleStringProperty(tblindex14_Part);
         this.tblindex09_Part = new SimpleStringProperty(tblindex09_Part);
         this.tblindex08_Part = new SimpleStringProperty(tblindex08_Part);
         this.tblindex05_Part = new SimpleStringProperty(tblindex05_Part);
         this.tblindex06_Part = new SimpleStringProperty(tblindex06_Part);
-        this.tblindex11_Part = new SimpleStringProperty(tblindex11_Part);
     }
 
     public String getTblPartsRow() {
@@ -55,6 +56,14 @@ public class VSPTablePartList {
 
     public void setTblindex01_Part(String tblindex01_Part) {
         this.tblindex01_Part.set(tblindex01_Part);
+    }
+
+    public String getTblindex03_Part() {
+        return tblindex03_Part.get();
+    }
+
+    public void setTblindex03_Part(String tblindex03_Part) {
+        this.tblindex03_Part.set(tblindex03_Part);
     }
 
     public String getTblindex14_Part() {
@@ -95,14 +104,6 @@ public class VSPTablePartList {
 
     public void setTblindex05_Part(String tblindex05_Part) {
         this.tblindex05_Part.set(tblindex05_Part);
-    }
-
-    public String getTblindex11_Part() {
-        return tblindex11_Part.get();
-    }
-
-    public void setTblindex11_Part(String tblindex11_Part) {
-        this.tblindex11_Part.set(tblindex11_Part);
     }
 
 }

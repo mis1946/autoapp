db89f5ac8613b6be4881b27886c8b04b8f08684d
myst <johndaveaquino35@gmail.com>
2023-11-08 09:21:20 +0800

    Update ActivityFormController.java

--- src/org/rmj/auto/app/views/ActivityFormController.java
+++ src/org/rmj/auto/app/views/ActivityFormController.java
@@ -78,7 +78,7 @@ public class ActivityFormController implements Initializable, ScreenInterface {
     private ObservableList<ActivityVchlEntryTable> actVhclModelData = FXCollections.observableArrayList();
     unloadForm unload = new unloadForm(); //Used in Close Button
     CancelForm cancelform = new CancelForm(); //Object for closing form
-    private final String pxeModuleName = "Activity Entry"; //Form Title
+    private final String pxeModuleName = "Activity Information"; //Form Title
     private int pnEditMode;//Modifying fields
     @FXML
     private Button btnAdd;

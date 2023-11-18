721373a1c2f163b98f96eb06726590c34fc8a1ec
r-sie <32829606+Arsiela@users.noreply.github.com>
2023-11-18 08:56:12 +0800

    Refactor Input text formatter
    - Refactor Input text formatter

--- src/org/rmj/auto/app/views/InputTextFormatter.java
+++ src/org/rmj/auto/app/views/InputTextFormatter.java
@@ -5,34 +5,73 @@
  */
 package org.rmj.auto.app.views;
 
+import java.text.NumberFormat;
 import java.util.function.UnaryOperator;
 import java.util.regex.Pattern;
 import javafx.scene.control.TextFormatter;
+import static org.rmj.appdriver.agentfx.CommonUtils.NumberFormat;
 
 /**
  * Date : 3/14/2023
  * @author Arsiela
  */
+
 public class InputTextFormatter extends TextFormatter<String> {
 
+//    public InputTextFormatter() {
+//        super(new NumericUnaryOperator());
+//    }
+    
     public InputTextFormatter(Pattern pattern) {
         super(new NumericUnaryOperator(pattern));
     }
 
+
     private static class NumericUnaryOperator implements UnaryOperator<TextFormatter.Change> {
 
+        //private final Pattern pattern = Pattern.compile("^\\d*(,\\d{0,2})?(\\.\\d{0,2})?$");
+        
         private final Pattern pattern;
 
         public NumericUnaryOperator(Pattern pattern) {
             this.pattern = pattern;
         }
-
+        
         @Override
         public TextFormatter.Change apply(TextFormatter.Change change) {
-            if (!pattern.matcher(change.getControlNewText()).matches()) {
+            if (pattern.matcher(change.getControlNewText()).matches()) {
+                //System.out.println("Accepted: " + change.getControlNewText());
+                return change;
+            } else {
+                //System.out.println("Rejected: " + change.getControlNewText());
                 return null;
             }
-            return change;
         }
     }
+   
 }
+
+
+//public class InputTextFormatter extends TextFormatter<String> {
+//
+//    public InputTextFormatter(Pattern pattern) {
+//        super(new NumericUnaryOperator(pattern));
+//    }
+//
+//    private static class NumericUnaryOperator implements UnaryOperator<TextFormatter.Change> {
+//
+//        private final Pattern pattern;
+//
+//        public NumericUnaryOperator(Pattern pattern) {
+//            this.pattern = pattern;
+//        }
+//
+//        @Override
+//        public TextFormatter.Change apply(TextFormatter.Change change) {
+//            if (!pattern.matcher(change.getControlNewText()).matches()) {
+//                return null;
+//            }
+//            return change;
+//        }
+//    }
+//}

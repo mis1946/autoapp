18a1eb340cbd4f3483ea5f5a394b63fbb725f532
myst <johndaveaquino35@gmail.com>
2023-11-20 16:53:12 +0800

    Create UnitDeliveryReceiptMasterList.java

---
+++ src/org/rmj/auto/app/sales/UnitDeliveryReceiptMasterList.java
@@ -0,0 +1,459 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.rmj.auto.app.sales;
+
+import javafx.beans.property.SimpleStringProperty;
+
+/**
+ *
+ * @author User
+ */
+public class UnitDeliveryReceiptMasterList {
+
+    private SimpleStringProperty tblUDRMaster_Row;
+    private SimpleStringProperty tblindex01_Master;
+    private SimpleStringProperty tblindex02_Master;
+    private SimpleStringProperty tblindex03_Master;
+    private SimpleStringProperty tblindex04_Master;
+    private SimpleStringProperty tblindex05_Master;
+    private SimpleStringProperty tblindex06_Master;
+    private SimpleStringProperty tblindex07_Master;
+    private SimpleStringProperty tblindex08_Master;
+    private SimpleStringProperty tblindex09_Master;
+    private SimpleStringProperty tblindex10_Master;
+    private SimpleStringProperty tblindex11_Master;
+    private SimpleStringProperty tblindex12_Master;
+    private SimpleStringProperty tblindex13_Master;
+    private SimpleStringProperty tblindex14_Master;
+    private SimpleStringProperty tblindex15_Master;
+    private SimpleStringProperty tblindex16_Master;
+    private SimpleStringProperty tblindex17_Master;
+    private SimpleStringProperty tblindex18_Master;
+    private SimpleStringProperty tblindex19_Master;
+    private SimpleStringProperty tblindex20_Master;
+    private SimpleStringProperty tblindex21_Master;
+    private SimpleStringProperty tblindex22_Master;
+    private SimpleStringProperty tblindex23_Master;
+    private SimpleStringProperty tblindex24_Master;
+    private SimpleStringProperty tblindex25_Master;
+    private SimpleStringProperty tblindex26_Master;
+    private SimpleStringProperty tblindex27_Master;
+    private SimpleStringProperty tblindex28_Master;
+    private SimpleStringProperty tblindex29_Master;
+    private SimpleStringProperty tblindex30_Master;
+    private SimpleStringProperty tblindex31_Master;
+    private SimpleStringProperty tblindex32_Master;
+    private SimpleStringProperty tblindex33_Master;
+    private SimpleStringProperty tblindex34_Master;
+    private SimpleStringProperty tblindex35_Master;
+    private SimpleStringProperty tblindex36_Master;
+    private SimpleStringProperty tblindex37_Master;
+    private SimpleStringProperty tblindex38_Master;
+    private SimpleStringProperty tblindex39_Master;
+
+    UnitDeliveryReceiptMasterList(String tblUDRMaster_Row,
+            String tblindex01_Master,
+            String tblindex02_Master,
+            String tblindex03_Master,
+            String tblindex04_Master,
+            String tblindex05_Master,
+            String tblindex06_Master,
+            String tblindex07_Master,
+            String tblindex08_Master,
+            String tblindex09_Master,
+            String tblindex10_Master,
+            String tblindex11_Master,
+            String tblindex12_Master,
+            String tblindex13_Master,
+            String tblindex14_Master,
+            String tblindex15_Master,
+            String tblindex16_Master,
+            String tblindex17_Master,
+            String tblindex18_Master,
+            String tblindex19_Master,
+            String tblindex20_Master,
+            String tblindex21_Master,
+            String tblindex22_Master,
+            String tblindex23_Master,
+            String tblindex24_Master,
+            String tblindex25_Master,
+            String tblindex26_Master,
+            String tblindex27_Master,
+            String tblindex28_Master,
+            String tblindex29_Master,
+            String tblindex30_Master,
+            String tblindex31_Master,
+            String tblindex32_Master,
+            String tblindex33_Master,
+            String tblindex34_Master,
+            String tblindex35_Master,
+            String tblindex36_Master,
+            String tblindex37_Master,
+            String tblindex38_Master,
+            String tblindex39_Master) {
+        this.tblUDRMaster_Row = new SimpleStringProperty(tblUDRMaster_Row);
+        this.tblindex01_Master = new SimpleStringProperty(tblindex01_Master);
+        this.tblindex02_Master = new SimpleStringProperty(tblindex02_Master);
+        this.tblindex03_Master = new SimpleStringProperty(tblindex03_Master);
+        this.tblindex04_Master = new SimpleStringProperty(tblindex04_Master);
+        this.tblindex05_Master = new SimpleStringProperty(tblindex05_Master);
+        this.tblindex06_Master = new SimpleStringProperty(tblindex06_Master);
+        this.tblindex07_Master = new SimpleStringProperty(tblindex07_Master);
+        this.tblindex08_Master = new SimpleStringProperty(tblindex08_Master);
+        this.tblindex09_Master = new SimpleStringProperty(tblindex09_Master);
+        this.tblindex10_Master = new SimpleStringProperty(tblindex10_Master);
+        this.tblindex11_Master = new SimpleStringProperty(tblindex11_Master);
+        this.tblindex12_Master = new SimpleStringProperty(tblindex12_Master);
+        this.tblindex13_Master = new SimpleStringProperty(tblindex13_Master);
+        this.tblindex14_Master = new SimpleStringProperty(tblindex14_Master);
+        this.tblindex15_Master = new SimpleStringProperty(tblindex15_Master);
+        this.tblindex16_Master = new SimpleStringProperty(tblindex16_Master);
+        this.tblindex17_Master = new SimpleStringProperty(tblindex17_Master);
+        this.tblindex18_Master = new SimpleStringProperty(tblindex18_Master);
+        this.tblindex19_Master = new SimpleStringProperty(tblindex19_Master);
+        this.tblindex20_Master = new SimpleStringProperty(tblindex20_Master);
+        this.tblindex21_Master = new SimpleStringProperty(tblindex21_Master);
+        this.tblindex22_Master = new SimpleStringProperty(tblindex22_Master);
+        this.tblindex23_Master = new SimpleStringProperty(tblindex23_Master);
+        this.tblindex24_Master = new SimpleStringProperty(tblindex24_Master);
+        this.tblindex25_Master = new SimpleStringProperty(tblindex25_Master);
+        this.tblindex26_Master = new SimpleStringProperty(tblindex26_Master);
+        this.tblindex27_Master = new SimpleStringProperty(tblindex27_Master);
+        this.tblindex28_Master = new SimpleStringProperty(tblindex28_Master);
+        this.tblindex29_Master = new SimpleStringProperty(tblindex29_Master);
+        this.tblindex30_Master = new SimpleStringProperty(tblindex30_Master);
+        this.tblindex31_Master = new SimpleStringProperty(tblindex31_Master);
+        this.tblindex32_Master = new SimpleStringProperty(tblindex32_Master);
+        this.tblindex33_Master = new SimpleStringProperty(tblindex33_Master);
+        this.tblindex34_Master = new SimpleStringProperty(tblindex34_Master);
+        this.tblindex35_Master = new SimpleStringProperty(tblindex35_Master);
+        this.tblindex36_Master = new SimpleStringProperty(tblindex36_Master);
+        this.tblindex37_Master = new SimpleStringProperty(tblindex37_Master);
+        this.tblindex38_Master = new SimpleStringProperty(tblindex38_Master);
+        this.tblindex39_Master = new SimpleStringProperty(tblindex39_Master);
+
+    }
+
+    public String getTblUDRMaster_Row() {
+        return tblUDRMaster_Row.get();
+    }
+
+    public void getTblUDRMaster_Row(String tblUDRMaster_Row) {
+        this.tblUDRMaster_Row.set(tblUDRMaster_Row);
+    }
+
+    public String getTblindex01_Master() {
+        return tblindex01_Master.get();
+    }
+
+    public void setTblindex01_Master(String tblindex01_Master) {
+        this.tblindex01_Master.set(tblindex01_Master);
+    }
+
+    public String getTblindex02_Master() {
+        return tblindex02_Master.get();
+    }
+
+    public void setTblindex02_Master(String tblindex02_Master) {
+        this.tblindex02_Master.set(tblindex02_Master);
+    }
+
+    public String getTblindex03_Master() {
+        return tblindex03_Master.get();
+    }
+
+    public void setTblindex03_Master(String tblindex03_Master) {
+        this.tblindex03_Master.set(tblindex03_Master);
+    }
+
+    public String getTblindex04_Master() {
+        return tblindex04_Master.get();
+    }
+
+    public void setTblindex04_Master(String tblindex04_Master) {
+        this.tblindex04_Master.set(tblindex04_Master);
+    }
+
+    public String getTblindex05_Master() {
+        return tblindex05_Master.get();
+    }
+
+    public void setTblindex05_Master(String tblindex05_Master) {
+        this.tblindex05_Master.set(tblindex05_Master);
+    }
+
+    public String getTblindex06_Master() {
+        return tblindex06_Master.get();
+    }
+
+    public void setTblindex06_Master(String tblindex06_Master) {
+        this.tblindex06_Master.set(tblindex06_Master);
+    }
+
+    public String getTblindex07_Master() {
+        return tblindex07_Master.get();
+    }
+
+    public void setTblindex07_Master(String tblindex07_Master) {
+        this.tblindex07_Master.set(tblindex07_Master);
+    }
+
+    public String getTblindex08_Master() {
+        return tblindex08_Master.get();
+    }
+
+    public void setTblindex08_Master(String tblindex08_Master) {
+        this.tblindex08_Master.set(tblindex08_Master);
+    }
+
+    public String getTblindex09_Master() {
+        return tblindex09_Master.get();
+    }
+
+    public void setTblindex09_Master(String tblindex09_Master) {
+        this.tblindex09_Master.set(tblindex09_Master);
+    }
+
+    public String getTblindex10_Master() {
+        return tblindex10_Master.get();
+    }
+
+    public void setTblindex10_Master(String tblindex10_Master) {
+        this.tblindex10_Master.set(tblindex10_Master);
+    }
+
+    public String getTblindex11_Master() {
+        return tblindex11_Master.get();
+    }
+
+    public void setTblindex11_Master(String tblindex11_Master) {
+        this.tblindex11_Master.set(tblindex11_Master);
+    }
+
+    public String getTblindex12_Master() {
+        return tblindex12_Master.get();
+    }
+
+    public void setTblindex12_Master(String tblindex12_Master) {
+        this.tblindex12_Master.set(tblindex12_Master);
+    }
+
+    public String tblindex13_Master() {
+        return tblindex13_Master.get();
+    }
+
+    public void setTblindex13_Master(String tblindex13_Master) {
+        this.tblindex13_Master.set(tblindex13_Master);
+    }
+
+    public String getTblindex14_Master() {
+        return tblindex14_Master.get();
+    }
+
+    public void setTblindex14_Master(String tblindex14_Master) {
+        this.tblindex14_Master.set(tblindex14_Master);
+    }
+
+    public String getTblindex15_Master() {
+        return tblindex15_Master.get();
+    }
+
+    public void setTblindex15_Master(String tblindex15_Master) {
+        this.tblindex15_Master.set(tblindex15_Master);
+    }
+
+    public String getTblindex16_Master() {
+        return tblindex16_Master.get();
+    }
+
+    public void setTblindex16_Master(String tblindex16_Master) {
+        this.tblindex16_Master.set(tblindex16_Master);
+    }
+
+    public String getTblindex17_Master() {
+        return tblindex17_Master.get();
+    }
+
+    public void setTblindex17_Master(String tblindex17_Master) {
+        this.tblindex17_Master.set(tblindex17_Master);
+    }
+
+    public String getTblindex18_Master() {
+        return tblindex18_Master.get();
+    }
+
+    public void setTblindex18_Master(String tblindex18_Master) {
+        this.tblindex18_Master.set(tblindex18_Master);
+    }
+
+    public String getTblindex19_Master() {
+        return tblindex19_Master.get();
+    }
+
+    public void setTblindex19_Master(String tblindex19_Master) {
+        this.tblindex19_Master.set(tblindex19_Master);
+    }
+
+    public String getTblindex20_Master() {
+        return tblindex20_Master.get();
+    }
+
+    public void setTblindex20_Master(String tblindex20_Master) {
+        this.tblindex20_Master.set(tblindex20_Master);
+    }
+
+    public String getTblindex21_Master() {
+        return tblindex21_Master.get();
+    }
+
+    public void setTblindex21_Master(String tblindex21_Master) {
+        this.tblindex21_Master.set(tblindex21_Master);
+    }
+
+    public String getTblindex22_Master() {
+        return tblindex22_Master.get();
+    }
+
+    public void setTblindex22_Master(String tblindex22_Master) {
+        this.tblindex22_Master.set(tblindex22_Master);
+    }
+
+    public String getTblindex23_Master() {
+        return tblindex23_Master.get();
+    }
+
+    public void setTblindex23_Master(String tblindex23_Master) {
+        this.tblindex23_Master.set(tblindex23_Master);
+    }
+
+    public String getTblindex24_Master() {
+        return tblindex24_Master.get();
+    }
+
+    public void setTblindex24_Master(String tblindex24_Master) {
+        this.tblindex24_Master.set(tblindex24_Master);
+    }
+
+    public String getTblindex25_Master() {
+        return tblindex25_Master.get();
+    }
+
+    public void setTblindex25_Master(String tblindex25_Master) {
+        this.tblindex25_Master.set(tblindex25_Master);
+    }
+
+    public String getTblindex26_Master() {
+        return tblindex26_Master.get();
+    }
+
+    public void setTblindex26_Master(String tblindex26_Master) {
+        this.tblindex26_Master.set(tblindex26_Master);
+    }
+
+    public String getTblindex27_Master() {
+        return tblindex27_Master.get();
+    }
+
+    public void setTblindex27_Master(String tblindex27_Master) {
+        this.tblindex27_Master.set(tblindex27_Master);
+    }
+
+    public String getTblindex28_Master() {
+        return tblindex28_Master.get();
+    }
+
+    public void setTblindex28_Master(String tblindex28_Master) {
+        this.tblindex28_Master.set(tblindex28_Master);
+    }
+
+    public String getTblindex29_Master() {
+        return tblindex29_Master.get();
+    }
+
+    public void setTblindex29_Master(String tblindex29_Master) {
+        this.tblindex29_Master.set(tblindex29_Master);
+    }
+
+    public String getTblindex30_Master() {
+        return tblindex30_Master.get();
+    }
+
+    public void setTblindex30_Master(String tblindex30_Master) {
+        this.tblindex30_Master.set(tblindex30_Master);
+    }
+
+    public String getTblindex31_Master() {
+        return tblindex31_Master.get();
+    }
+
+    public void setTblindex31_Master(String tblindex31_Master) {
+        this.tblindex31_Master.set(tblindex31_Master);
+    }
+
+    public String getTblindex32_Master() {
+        return tblindex32_Master.get();
+    }
+
+    public void setTblindex32_Master(String tblindex32_Master) {
+        this.tblindex32_Master.set(tblindex32_Master);
+    }
+
+    public String getTblindex33_Master() {
+        return tblindex33_Master.get();
+    }
+
+    public void setTblindex33_Master(String tblindex33_Master) {
+        this.tblindex33_Master.set(tblindex33_Master);
+    }
+
+    public String getTblindex34_Master() {
+        return tblindex34_Master.get();
+    }
+
+    public void setTblindex34_Master(String tblindex34_Master) {
+        this.tblindex34_Master.set(tblindex34_Master);
+    }
+
+    public String getTblindex35_Master() {
+        return tblindex35_Master.get();
+    }
+
+    public void setTblindex35_Master(String tblindex35_Master) {
+        this.tblindex35_Master.set(tblindex35_Master);
+    }
+
+    public String getTblindex36_Master() {
+        return tblindex36_Master.get();
+    }
+
+    public void setTblindex36_Master(String tblindex36_Master) {
+        this.tblindex36_Master.set(tblindex36_Master);
+    }
+
+    public String getTblindex37_Master() {
+        return tblindex37_Master.get();
+    }
+
+    public void setTblindex37_Master(String tblindex37_Master) {
+        this.tblindex37_Master.set(tblindex37_Master);
+    }
+
+    public String getTblindex38_Master() {
+        return tblindex38_Master.get();
+    }
+
+    public void setTblindex38_Master(String tblindex38_Master) {
+        this.tblindex38_Master.set(tblindex38_Master);
+    }
+
+    public String getTblindex39_Master() {
+        return tblindex39_Master.get();
+    }
+
+    public void setTblindex39_Master(String tblindex39_Master) {
+        this.tblindex39_Master.set(tblindex39_Master);
+    }
+
+}

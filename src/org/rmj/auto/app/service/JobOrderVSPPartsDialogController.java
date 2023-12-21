f84196145656d0db18a6abc3ea5bb05ac42eb7a8
myst <johndaveaquino35@gmail.com>
2023-12-21 16:52:41 +0800

    Update JobOrderVSPPartsDialogController.java

--- src/org/rmj/auto/app/service/JobOrderVSPPartsDialogController.java
+++ src/org/rmj/auto/app/service/JobOrderVSPPartsDialogController.java
@@ -17,7 +17,6 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
-import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;

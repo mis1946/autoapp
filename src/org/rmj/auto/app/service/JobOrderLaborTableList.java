69e5749ffcb41562fccbc1f1a163b0898fa9ec7e
myst <johndaveaquino35@gmail.com>
2024-01-22 11:30:11 +0800

    Update JobOrderLaborTableList.java

--- src/org/rmj/auto/app/service/JobOrderLaborTableList.java
+++ src/org/rmj/auto/app/service/JobOrderLaborTableList.java
@@ -4,7 +4,10 @@
  */
 package org.rmj.auto.app.service;
 
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleStringProperty;
+import javafx.scene.control.CheckBox;
 
 public class JobOrderLaborTableList {
 
@@ -13,17 +16,24 @@ public class JobOrderLaborTableList {
     private SimpleStringProperty tblindex10_Labor; // sLaborDsc
     private SimpleStringProperty tblindex03_Labor; // sPayChrge
     private SimpleStringProperty tblindex06_Labor; //nUnitPrce
+    private SimpleBooleanProperty tblindex03; // sPayChrge
+    private CheckBox FreeOrNot;
 
     JobOrderLaborTableList(String tblLaborRow,
             String tblindex04_Labor,
             String tblindex10_Labor,
             String tblindex03_Labor,
-            String tblindex06_Labor) {
+            String tblindex06_Labor,
+            boolean tblindex03) {
         this.tblLaborRow = new SimpleStringProperty(tblLaborRow);
         this.tblindex04_Labor = new SimpleStringProperty(tblindex04_Labor);
         this.tblindex10_Labor = new SimpleStringProperty(tblindex10_Labor);
         this.tblindex03_Labor = new SimpleStringProperty(tblindex03_Labor);
         this.tblindex06_Labor = new SimpleStringProperty(tblindex06_Labor);
+        this.tblindex03 = new SimpleBooleanProperty(tblindex03);
+        this.FreeOrNot = new CheckBox();
+        this.FreeOrNot.setSelected(tblindex03);
+        FreeOrNot.setDisable(true);
     }
 
     public String getTblLaborRow() {
@@ -64,6 +74,26 @@ public class JobOrderLaborTableList {
 
     public void setTblindex06_Labor(String tblindex06_Labor) {
         this.tblindex06_Labor.set(tblindex06_Labor);
+    }
+
+    public CheckBox getFreeOrNot() {
+        return FreeOrNot;
+    }
+
+    public void setFreeOrNot(CheckBox FreeOrNot) {
+        this.FreeOrNot = FreeOrNot;
+    }
+
+    public boolean isTblindex03() {
+        return tblindex03.get();
+    }
+
+    public void setTblindex03(boolean tblindex03) {
+        this.tblindex03.set(tblindex03);
+    }
+
+    public BooleanProperty selectedProperty3() {
+        return tblindex03;
     }
 
 }
